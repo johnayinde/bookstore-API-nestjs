a6be9e4579c6ca09228a9e5f8ee68cd1eaf92f6e
Ayinde John <lolaayinde@gmail.com>
2022-07-08 14:53:54 +0100

    added swagger package for documentation

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('api');
 
-  // app.use(
-  //   session({
-  //     secret: 'my express secret',
-  //     resave: false,
-  //     saveUnitialized: false,
-  //     cookie: {
-  //       maxAge: 60000,
-  //     },
-  //   }),
-  // );
-  // app.use(passport.initialize());
-  // app.use(passport.session());
+  const config = new DocumentBuilder()
+    .setTitle('Online Bookstore API')
+    .setDescription(
+      'An API that exposed all endpoints to create delete edit a book',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
